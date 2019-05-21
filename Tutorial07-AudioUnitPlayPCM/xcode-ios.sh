54f34e77f84b83f55e658755b2824672fb99a9fc
guo122 <balanuard@163.com>
2019-05-21 11:01:50 +0800

    [cmake] update cmake

--- Tutorial07-AudioUnitPlayPCM/xcode-ios.sh
+++ Tutorial07-AudioUnitPlayPCM/xcode-ios.sh
@@ -1,6 +1,6 @@
 
 cmake -E make_directory "build-ios" && cmake -E chdir "build-ios" cmake -G "Xcode" ../ -DCMAKE_TOOLCHAIN_FILE=../../toolchain/ios/iOS.toolchain.cmake -DPLATFORM=OS64COMBINED
 
-if [ -d build-ios/LearnVideoToolBox.xcodeproj ] ; then
-	open build-ios/LearnVideoToolBox.xcodeproj
+if [ "$?" == 0 ] ; then
+	open build-ios/*.xcodeproj
 fi
